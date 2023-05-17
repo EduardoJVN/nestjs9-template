03d5782a75eea52e5f269c202d3baaf3421830e7
EduardoJvn <ejvn30@gmail.com>
2023-05-17 11:16:08 -0300

    feat: add gralph

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+const SSL = process.env.DB_SSL == 1 ? true : false;
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  ssl: SSL,
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
